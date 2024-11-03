e5bd185d038f675c84b6212e56477ed1d1b49a05
JosemaCB <contactojosema7@gmail.com>
2024-11-03 13:10:27 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ZeldaApp',
+  webDir: 'www'
+};
+
+export default config;
